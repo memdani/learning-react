b3482209dc66d463bfbc523307fc67b7786c7ea2
memdani <memdaniy12@gmail.com>
2022-05-18 17:53:10 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import React from 'react';
 import {Footer, Blog, Possibility, Features, What, Header} from './containers';
 import {CTA, Brand, Navbar} from './components';
 
+import './App.css';
+
 export const App = () => {
   return (
     <div>
-        <div className='App'>
+        <div>
           <div className='gradient__bg'>
             <Navbar />
             <Header />
